0787cc57722b93d3e4408b3490534735ef4ddc80
Elias <wileld17@htl-kaindorf.ac.at>
2021-05-19 13:02:39 +0200

    created first websocket

---
+++ src/utils/websocket.ts
@@ -0,0 +1,13 @@
+
+
+
+export const CustomWebSocket = () =>{
+    const SOCKET_URL = 'http://localhost:8080/ws-message';
+
+    const connect = () =>{
+        const ws: WebSocket = new WebSocket(SOCKET_URL); 
+        
+        
+    }
+
+}
